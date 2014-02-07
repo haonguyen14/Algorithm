13360fa6841469134004cea470d8019c86b836ec
haonguyen14 <hao_nguyen1412@yahoo.com.vn>
2014-02-07 14:23:07 -0800

    Tree Traversal

---
+++ Algorithms/InorderTraversal.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+
+public InorderTraversal {
+	
+	/*
+	 *	Recursive Version
+	 */
+	 public static void traverse(Tree<T> tree) {
+		if(tree == null)
+			return;
+
+		travesrse(tree.mLeft);
+		System.out.print(tree.mData + " ");
+		traverse(tree.mRight);
+	 }
+
+
+
+	 /*
+	  *	Iterative Version
+	  */
+	  public static void traverse(Tree<T> tree) {
+		Stack<Tree<T>> stack = new Stack<Tree<T>>();
+
+		Tree<T> current = tree;
+		do {
+			if(current != null) {
+				stack.push(current);
+				current = current.mLeft;
+			} else {
+				current = stack.pop(); // root
+				System.out.println(current.mData);
+				current = current.mRight;
+			}
+		} while(!stack.isEmpty());
+	  }
+}
