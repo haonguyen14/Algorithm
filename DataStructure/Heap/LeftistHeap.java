d3e3e226aa99b6c1ffaa5d5ab48381d41e229c6e
haonguyen14 <hao_nguyen1412@yahoo.com.vn>
2014-02-08 15:20:17 -0800

    LeftistHeap's Merging is now non-destructive

--- DataStructure/Heap/LeftistHeap.java
+++ DataStructure/Heap/LeftistHeap.java
@@ -2,27 +2,23 @@ public class LeftistHeap<T> implements Heap<T> {
 	/*
 	 *	Simple Binary Tree Implementation
 	 */
-	private class Tree<T> {
-		public T mData;
+	public T mData;
 
-		public Tree<T> mLeft;
-		public Tree<T> mRight;
+	public LeftistHeap<T> mLeft;
+	public LeftistHeap<T> mRight;
 
 
-		public Tree(T data) {
-			this.mData = data;
-		}
-
-		
-		public Tree(T data, Tree<T> left, Tree<T> right) {
-			this.mData = data;
-			this.mLeft = left;
-			this.mRight = right;
-		}
+	public LeftistHeap(T data) {
+		this.mData = data;
 	}
 
+		
+	public LeftistHeap(T data, LeftistHeap<T> left, LeftistHeap<T> right) {
+		this.mData = data;
+		this.mLeft = left;
+		this.mRight = right;
+	}
 	
-	Tree<T> mTree;
 
 
 	/*
@@ -30,48 +26,43 @@ public class LeftistHeap<T> implements Heap<T> {
 	 *	Construct leftist heap
 	 */
 	public LeftistHeap(T data) {
-		this.mTree = new Tree<T>(data);
+		this.mData = data;
 	}
 
-
-	/*
-	 *	Constructor
-	 *	Construct leftist heap from an array
-	 *	Runtime: 
-	 */
-	public LeftistHeap(T[] input) {
-		this.mTree = new Tree<T>(t[0]);	
-	}
 
 
 	/*
 	 *	Merge two leftist heaps
 	 *	Runtime: 
 	 */
-	 public void merge(LeftistHeap<T> heap) {
-		if(heap == null)
-			return;
+	 public static <T extends Comparable<T>> LeftistHeap<T> merge(LeftistHeap<T> h1, LeftistHeap<T> h2) {
+	 	LeftistHeap<T> newTree = null;
+		LeftistHeap<T> h1Clone = h1 != null ? h1.clone() : null;
+		LeftistHeap<T> h2Clone = h2 != null ? h2.clone() : null;
 
-		/* Compare Two Root Node */
-		if(this.mTree.mData.compareTo(heap.mData) < 0) {
-			heap.merge(this.mRight);
-			this.mRight = heap;
+		/* Edge Condition */
+		if(h1 == null || h2 == null)
+			return h1 != null ? h1 : h2;
+		
+		/* Merge */
+		if(h1.mData.compareTo(h2.mData) < 0) {
+			newTree = h1;
+			newTree.mRight = LeftistHeap.merge(newTree.mRight, h2)
 		} else {
-			this.merge(heap.mRight);
-			heap.mRight = this;
-			this = heap;
+			newTree = h2;
+			newTree.mRight = LeftistHeap.merge(newTree.mRight, h1)
 		}
 
+		
 		/* Fix */
-		int leftNullPath = (this.mLeft == null) ? -1 : this.mLeft.getNullPathLength();
-		int rightNullPath = (this.mRight == null) ? -1 : this.mRight.getNullPathLength();
+		if(newTree.mLeft.getNullPathLength() < newTree.mRight.getNullPathLength()) {
+			LeftistTree<T> temp = newTree.mLeft;
 
-		if(leftNullPath < rightNullPath) {
-			LeftistHeap<T> temp = this.mLeft;
-
-			this.mLeft = this.mRight;
-			this.mRight = temp;
+			newTree.mLeft = newTree.mRight;
+			newTree.mRight = temp;
 		}
+
+		return newTree;
 	 }
 
 
@@ -80,22 +71,8 @@ public class LeftistHeap<T> implements Heap<T> {
 	 *	Runtime:
 	 */
 	 public void insert(T data) {
-		this.merge(new LeftistHeap(data));
+		this = LeftistTree.merge(new LeftistHeap(data), this);
 	 }
-
-
-	 /*
-	  *	Remove min/max
-	  *	Runtime:
-	  */
-	  public T remove() {
-		T ret = this.mTree.mData;
-
-		this.mLeft.merge(this.mRight);
-		this = mLeft;
-
-		return ret;
-	  }
 
 
 	  /*
@@ -103,7 +80,9 @@ public class LeftistHeap<T> implements Heap<T> {
 	   *	Runtime: O(1)
 	   */
 	   public T extract() {
-		return this.mTree.mData;
+	   	T ret = this.mData;
+		this = LeftistHeap.merge(this.mLeft, this.mRight);
+		return ret;
 	   }
 
 
@@ -113,5 +92,13 @@ public class LeftistHeap<T> implements Heap<T> {
 		else
 			return ((this.mLeft.getNullPathLength() > this.mRight.getNullPathLength()) ?
 				this.mRight.getNullPathLength() : this.mLeft.getNullPathLength()) + 1;
+	   }
+
+
+	   private <T extends Comparable<T>> LeftistHeap<T> clone() {
+		LeftistHeap<T> left = this.mLeft != null ? this.mLeft.clone() : null;
+		LeftistHeap<T> right = this.mRight != null ? this.mRight.clone() : null;
+
+		return new LeftistHeap<T>(this.mData, left, right);
 	   }
 }
